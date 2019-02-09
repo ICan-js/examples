95a8629b8b40a49e33f7a61ab85e03b1d4e3dbf3
Phelps <phelpsmemo@gmail.com>
2019-02-09 17:17:26 -0200

    Finalização da implementação da tela de calibração

--- controle-de-mouse/src/calibration.js
+++ controle-de-mouse/src/calibration.js
@@ -3,11 +3,16 @@
  * de gestos com a cabeça 
  */
 
+Swal.fire({
+    title: "Sobre a calibração",
+    text: "Nesta parte você deverá apontar seu nariz para cada um dos pontos e ao mesmo tempo clicar com o mouse sobre o ponto ao qual você está mirando o nariz"
+});
+
 (function() {
     let pointsInStore = 0;
     const calibrationOptions = {
         pointSize: 25,
-        pointsToStorage: 10
+        pointsToStorage: 5
     };
     
     // Configurando a barra de progresso
@@ -17,14 +22,29 @@
     
     const finishFunction = (model) => {
         // Salvando o modelo treinado na memória
-        sessionStorage.setItem("regressionModelICJS", JSON.stringify(model));
+        sessionStorage.setItem("regressionModelICJS", JSON.stringify(model.exportRegressionWeights()));
 
+        Swal.fire({
+            title: "Finalizado!",
+            text: "A calibração foi finalizada com sucesso!",
+            type: 'success',
+            showCancelButton: true,
+            confirmButtonColor: '#0089F1',
+            cancelButtonColor: '#DF0128',
+            confirmButtonText: 'Continuar',
+            cancelButtonText: 'Recalibrar'
+          }).then((result) => {
+            if (result.value) {
+                document.location.href = "./examples.html";
+            } else {
+                location.reload();
+            }
+          });
     };
     
     const updateFunction = (update) => {
         pointsInStore += 1;
- 
-        progressBar.value = (calibrationOptions.pointsToStorage) * (pointsInStore / 20);
+        progressBar.value = (calibrationOptions.pointsToStorage) * (pointsInStore / 10);
     };
     
     const regressionModel = new icjs.core.LinearRegression();
